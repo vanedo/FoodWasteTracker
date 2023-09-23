24130d1b74f741fc7a1400f84ccb748502dc1d7b
vanedo <ssul0017@student.monash.edu>
2023-09-23 19:43:29 +0200

    register food action model in admin

--- src/apps/tracker/admin.py
+++ src/apps/tracker/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from apps.tracker.models import FoodAdvice
+from apps.tracker.models import FoodAction
+
 # Library for importing and exporting data
 from import_export.admin import ImportExportModelAdmin
 
@@ -12,5 +14,7 @@ from import_export.admin import ImportExportModelAdmin
 # Import/export data via admin panel
 class adviceAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     ...
-
+class actionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    ...
 admin.site.register(FoodAdvice, adviceAdmin)
+admin.site.register(FoodAction, actionAdmin)
