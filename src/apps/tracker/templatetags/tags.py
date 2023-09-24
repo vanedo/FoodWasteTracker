6d0374a5032b4a42f16e1ea16930234cba121911
vanedo <ssul0017@student.monash.edu>
2023-09-24 22:34:43 +0200

    Revert "modified metric values/labels"
    This reverts commit 8c5af59a90488028f15150983940eeaab5ac48bc.

--- src/apps/tracker/templatetags/tags.py
+++ src/apps/tracker/templatetags/tags.py
@@ -12,11 +12,11 @@ def metric_contributions(request):
     return contributions
 
 @register.simple_tag
-def metric_red_emissions(request):
-    upcycled_count = FoodHistory.objects.filter(eaten='upcycled').count()
-    red_emissions = 0.5 * 2.5 * upcycled_count # assume 0.5kg per item, 2.5kg CO2 saved per kg
+def metric_emissions(request):
+    thrown_count = FoodHistory.objects.filter(eaten='thrown_away').count()
+    emissions = 0.5 * 2.5 * thrown_count # assume 0.5kg per item, 2.5kg CO2 saved per kg
 
-    return red_emissions
+    return emissions
 
 @register.simple_tag
 def metric_items_saved(request):
