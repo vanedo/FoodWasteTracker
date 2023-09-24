0eee9b59ce2b40a706708d63ce94eefb63d0d1bc
vanedo <ssul0017@student.monash.edu>
2023-09-24 21:51:19 +0200

    cleanup. fn not finished

--- src/apps/tracker/forms.py
+++ src/apps/tracker/forms.py
@@ -6,12 +6,13 @@ CHOICES=[('upcycled','Upcycle'),
          ('thrown_away','Throw it away')]
 
 class FoodHistoryForm(forms.ModelForm):
-    eaten = forms.ChoiceField(label= 'Action', choices=CHOICES, widget=forms.RadioSelect())
+    eaten = forms.ChoiceField(label= '', choices=CHOICES, widget=forms.RadioSelect())
     
     class Meta:
         model = FoodHistory
-        #exclude = ('food',)
-        readonly_fields=('food', )
-
         fields = '__all__'
-
+        #def __init__(self, *args, **kwargs):
+        #    super(FoodHistoryForm, self).__init__(*args, **kwargs)
+        #    # Hide specific fields using CSS
+        #    self.fields['food'].widget.attrs['readonly'] = True
+        #    self.fields['food'].widget.attrs['style'] = 'display: none;'
